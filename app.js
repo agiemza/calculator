cfba2f7ef8dc66f6f4d8471db178f5693c48e3d9
arkadiusz971 <arkadiusz.giemza97@gmail.com>
2022-06-26 11:06:54 +0200

    add math operations

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const values = {
-    screen: "",
+const memory = {
+    firstValue: 0,
+    secondValue: 0,
+    operation: null,
+    isListening: true,
 }
 
 const screen = document.querySelector(".screen-input")
@@ -7,24 +10,68 @@ const screen = document.querySelector(".screen-input")
 const numberButtons = document.querySelectorAll(".number-button")
 numberButtons.forEach(button => {
     button.addEventListener("click", e => {
-        values.screen += e.target.value
-        updateScreen(values.screen)
+        if (screen.value[0] === "0" && screen.value[1] !== ".") {
+            screen.value = screen.value.substring(1)
+        }
+        if (memory.isListening) {
+            screen.value += e.target.value
+        }
+        else {
+            screen.value = ""
+            memory.isListening = true
+            screen.value += e.target.value
+        }
+        memory.secondValue = screen.value
     })
 })
 
 const mathButtons = document.querySelectorAll(".math-button")
 mathButtons.forEach(button => {
     button.addEventListener("click", e => {
-        console.log(e.target.attributes["data-operator"].value)
+        memory.operation = e.target.attributes["data-operator"].value
+        memory.firstValue = screen.value
+        memory.isListening = false
     })
 })
 
 const clearButton = document.querySelector(".clear-button")
 clearButton.addEventListener("click", () => {
-    values.screen = ""
-    updateScreen(values.screen)
+    memory.firstValue = 0
+    memory.secondValue = 0
+    memory.operation = null
+    screen.value = ""
 })
 
-function updateScreen(value) {
-    screen.value = value
+const resultButton = document.querySelector(".result-button")
+resultButton.addEventListener("click", () => {
+    memory.isListening = false
+    operate()
+})
+
+
+function operate() {
+    const firstValue = parseFloat(memory.firstValue)
+    const secondValue = parseFloat(memory.secondValue)
+    switch (memory.operation) {
+        case "add":
+            memory.firstValue = firstValue + secondValue
+            break
+        case "substract":
+            memory.firstValue = firstValue - secondValue
+            break
+        case "multiply":
+            memory.firstValue = firstValue * secondValue
+            break
+        case "divide":
+            if (secondValue !== 0) {
+                memory.firstValue = firstValue / secondValue
+            } else {
+                memory.firstValue = "71830"
+            }
+            break
+        default:
+            console.log("no operation declared")
+            break
+    }
+    screen.value = memory.firstValue
 }
